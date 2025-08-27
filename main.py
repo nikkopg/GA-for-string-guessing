d107688e04621ca140be67973d5a29e18d17e7ff
Nikko P. Gunara <70200533+nikkopg@users.noreply.github.com>
2025-08-27 20:48:38 +0700

    chore: update function call

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ population_size = int(input('Population Size (integer>=4): '))
 mutation_rate = float(input('Mutation Rate (float, 0-1): '))
 
 GA = GeneticAlgorithm(target_text)
-best_gen, generation = GA.runGA(population_size, mutation_rate)
+best_gen, generation = GA.run(population_size, mutation_rate)
 
 print("GA results:")
 print(f'Target\t\t: {target_text}')
 print(f'Solution\t: {best_gen}')
+
 print(f'Generation\t: {generation}')
